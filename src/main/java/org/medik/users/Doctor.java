b6d14a429c6cd160fa75e8aeed94072f825d9cb6
Omar M Juarez <omarmju906p@hotmail.com>
2022-03-21 16:43:13 -0600

    Versión inicial.

---
+++ src/main/java/org/medik/users/Doctor.java
@@ -0,0 +1,31 @@
+package org.medik.users;
+
+public class Doctor {
+    private int id;
+    private String name;
+    private String speciality;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
