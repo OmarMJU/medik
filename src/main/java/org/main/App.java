a9aeff02f9ba666e8805acdf5aaa9b9b37b372a4
Omar M Juarez <omarmju906p@hotmail.com>
2022-03-21 20:41:12 -0600

    Add AvailableAppointment method from inner class.

--- src/main/java/org/main/App.java
+++ src/main/java/org/main/App.java
@@ -1,11 +1,27 @@
 package org.main;
 
-import org.medik.users.Doctor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.medik.users.Patient;
+import org.medik.users.Doctor;
+import java.util.Calendar;
 
 public class App {
+    private final static Logger LOGGER = LogManager.getLogger(App.class);
+
     public static void main( String[] args ) {
+        Calendar calendar = Calendar.getInstance();
+
         Doctor myDoctor = new Doctor("Panchita", "Pediatra");
         Patient myPatient = new Patient("Conchita", "conchita@mail.com");
+
+        myDoctor.addAvailableAppointment(calendar.getTime(), "4pm");
+        myDoctor.addAvailableAppointment(calendar.getTime(), "3pm");
+        myDoctor.addAvailableAppointment(calendar.getTime(), "2pm");
+        LOGGER.info(myDoctor.getAvailableAppointment());
+
+        for (Doctor.AvailableAppointment aa : myDoctor.getAvailableAppointment()) {
+            LOGGER.info(aa.getDate() + " - " + aa.getTime());
+        }
     }
 }
--- src/main/java/org/medik/users/Doctor.java
+++ src/main/java/org/medik/users/Doctor.java
@@ -2,12 +2,18 @@ package org.medik.users;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Doctor {
     private static final Logger LOGGER = LogManager.getLogger(Doctor.class);
-    private int id;
-    private String name;
+    private static final String FORMAT_DATE = "dd/MM/yyyy";
+
+    private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<AvailableAppointment>();
     private String speciality;
+    private String name;
+    private int id;
 
     public Doctor() {
         LOGGER.info("Creating a new Doctor.");
@@ -41,5 +47,50 @@ public class Doctor {
 
     public void setSpeciality(String speciality) {
         this.speciality = speciality;
+    }
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointment() {
+        return this.availableAppointments;
+    }
+
+    // Nested static class.
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getDate() {
+            SimpleDateFormat sdf = new SimpleDateFormat(FORMAT_DATE);
+            return sdf.format(this.date);
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
